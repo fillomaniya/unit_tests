873e876fe4326033092376e21c591df769ea5d88
Ilya <fillomaniya@mail.ru>
2024-02-05 15:19:38 +0300

    add hw3

---
+++ main/java/homework3/UserRepository.java
@@ -0,0 +1,33 @@
+package homework3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository {
+
+    // Тут можно хранить аутентифицированных пользователей
+    List<User> data = new ArrayList<>();
+
+    public void addUser(User user) {
+        if (user.isAuthenticate) {
+            data.add(user);
+        }
+    }
+
+    public boolean findByName(String username) {
+        for (User user : data) {
+            if (user.name.equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void unloginAllCommonUsers() {
+        for (User user : data) {
+            if (!user.isAdmin) {
+                user.isAuthenticate = false;
+            }
+        }
+    }
+}
